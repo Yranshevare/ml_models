96f1a81abcaaedad016984b706ee3dbdd7d28833
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-12-03 15:41:32 +0530

    done with backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pandas as pd
+import pickle
 
 app = Flask(__name__)
+
+model = pickle.load(open("car_price_model_final.pkl", "rb"))
+
+
+@app.post('/predict')
+def predict():
+    try:
+        data = request.get_json()
+
+        if isinstance(data["year"], int):
+            return jsonify({"error": "Year must be a string"}), 400
+        
+        if not isinstance(data["km_driven"], int):
+            return jsonify({"error": "km_driven must be an integer"}), 400
+
+        # print(model.feature_names_in_)  # order of features ['year' 'km_driven' 'fuel' 'company']
+        df = pd.DataFrame([{
+            "year": data["year"],
+            "km_driven": data["km_driven"],
+            "fuel": data["fuel"],
+            "company": data["company"]
+        }])
+
+        # Predict
+        prediction = model.predict(df)
+
+        ans = jsonify({"prediction": str(prediction)})
+
+        return ans
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 @app.route('/')
 def home():
     return "Hello, Flask!"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
